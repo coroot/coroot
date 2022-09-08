ff5c5ddbbc5f15218f068afb02a738d2f76fd865
Anton Petruhin <anton.petruhin@gmail.com>
2022-09-08 11:48:47 +0300

    style scrollbar

--- front/src/App.vue
+++ front/src/App.vue
@@ -203,4 +203,17 @@ a {
     font-size: inherit;
     font-weight: inherit;
 }
+
+*::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    background-color: #F5F5F5;
+}
+*::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+    background-color: #F5F5F5;
+}
+*::-webkit-scrollbar-thumb {
+    background-color: #757575;
+}
 </style>
