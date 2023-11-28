843d6907fdbdcc7ca9e37105f5c0c2065c07f01a
Anton Petruhin <anton.petruhin@gmail.com>
2023-11-28 16:39:20 +0300

    profiling: switch to the `coroot-node-agent` eBPF spy

--- model/container.go
+++ model/container.go
@@ -1,6 +1,10 @@
 package model
 
-import "github.com/coroot/coroot/timeseries"
+import (
+	"github.com/coroot/coroot/timeseries"
+	"regexp"
+	"strings"
+)
 
 type ContainerStatus string
 
@@ -49,4 +53,22 @@ func NewContainer(id, name string) *Container {
 		Name:             name,
 		ApplicationTypes: map[ApplicationType]bool{},
 	}
+}
+
+var (
+	deploymentPodRegex  = regexp.MustCompile(`(/k8s/[a-z0-9-]+/[a-z0-9-]+)-[0-9a-f]{1,10}-[bcdfghjklmnpqrstvwxz2456789]{5}/.+`)
+	daemonsetPodRegex   = regexp.MustCompile(`(/k8s/[a-z0-9-]+/[a-z0-9-]+)-[bcdfghjklmnpqrstvwxz2456789]{5}/.+`)
+	statefulsetPodRegex = regexp.MustCompile(`(/k8s/[a-z0-9-]+/[a-z0-9-]+)-\d+/.+`)
+)
+
+func ContainerIdToServiceName(containerId string) string {
+	if !strings.HasPrefix(containerId, "/k8s/") {
+		return containerId
+	}
+	for _, r := range []*regexp.Regexp{deploymentPodRegex, daemonsetPodRegex, statefulsetPodRegex} {
+		if g := r.FindStringSubmatch(containerId); len(g) == 2 {
+			return g[1]
+		}
+	}
+	return containerId
 }
