61ac8c56aba6d98540c5de74120bd1e5dc6b3b34
Anton Petruhin <anton.petruhin@gmail.com>
2025-03-19 15:08:18 +0400

    docs: add support banner

---
+++ docs/src/theme/DocItem/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Footer from '@theme-original/DocItem/Footer';
+import type FooterType from '@theme/DocItem/Footer';
+import type { WrapperProps } from '@docusaurus/types';
+
+type Props = WrapperProps<typeof FooterType>;
+
+export default function FooterWrapper(props: Props): JSX.Element {
+  return (
+    <>
+        <Footer {...props} />
+        <div className="card" style={{display: 'block', marginTop: '40px', textAlign: 'center', backgroundColor: 'unset'}}>
+            Looking for 24/7 support from the Coroot team? Subscribe to Coroot Enterprise:
+            <a href="https://coroot.com/account" target="_blank" className="primary-button" style={{marginLeft: '4px'}}>
+                Start free trial
+            </a>
+        </div>
+    </>
+  );
+}
