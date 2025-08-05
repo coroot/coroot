5e4d282c8d630b77e2c9ea913cac1309cbe1c17d
Chris9224 <kaiyuan9224@gmail.com>
2025-08-05 16:56:34 +0200

    fix: Allow Ops Genie URL to be overwritten
    * Context: https://www.atlassian.com/software/opsgenie/migration.

--- notifications/opsgenie.go
+++ notifications/opsgenie.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
 	"strings"
 
 	"github.com/coroot/coroot/db"
@@ -27,6 +28,11 @@ func NewOpsgenie(apiKey string, euInstance bool) *Opsgenie {
 	if euInstance {
 		cfg.OpsGenieAPIURL = client.API_URL_EU
 	}
+
+	if envURL := os.Getenv("OPS_GENIE_API_URL"); envURL != "" {
+		cfg.OpsGenieAPIURL = envURL
+	}
+
 	c, _ := alert.NewClient(cfg)
 	return &Opsgenie{client: c}
 }
